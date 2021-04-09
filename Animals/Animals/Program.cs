92d281fb56884a2daa86e3acb398c499178c6846
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-04-08 21:47:10 -0500

    Finished

---
+++ Animals/Animals/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"What animal would you like to hear? Dog, Cat, or Pig");
+            string answer = Console.ReadLine().ToLower();
+
+            string sound = speak(answer);
+            Console.WriteLine($"A {answer} makes the sound: {sound}!");
+        }
+
+        static string speak(string animal)
+        {
+            string sound;
+            if (animal.ToLower() == "dog")
+            {
+                sound = "woof";
+                return sound;
+            }
+            else if (animal.ToLower() == "cat")
+            {
+                sound = "meow";
+                return sound;
+            }
+            else if (animal.ToLower() == "pig")
+            {
+                sound = "oink";
+                return sound;
+            }
+            return "sound";
+        }
+    }
+}
